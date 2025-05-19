422def804b771ed13460387749099510d5c14281
Frankisael <113447118+Frankisael@users.noreply.github.com>
2025-05-19 01:19:08 -0500

    Update Scrippppads.js

--- Scrippppads.js
+++ Scrippppads.js
@@ -1,10 +1,10 @@
-document.write('<scr' + 'ipt type="text/javascript">' +
-'atOptions = {' +
-'  "key": "c9e3a93a071a563b36e503c69eda88ef",' +
-'  "format": "iframe",' +
-'  "height": 250,' +
-'  "width": 300,' +
-'  "params": {}' +
-'};' +
-'</scr' + 'ipt>' +
-'<scr' + 'ipt type="text/javascript" src="//abnormalitylovingmammal.com/c9e3a93a071a563b36e503c69eda88ef/invoke.js"></scr' + 'ipt>');
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'c9e3a93a071a563b36e503c69eda88ef',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//abnormalitylovingmammal.com/c9e3a93a071a563b36e503c69eda88ef/invoke.js"></script>
